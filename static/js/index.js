442f24d24df1e0a2a5e8f76fe1a458603e749e39
Lucas <lmdias93@gmail.com>
2022-08-28 09:15:40 +0200

    PlaylistTimemachine bugfix

---
+++ static/js/index.js
@@ -0,0 +1,56 @@
+const divIDs = ["portfolio", "about", "contact"]
+var currentlyOpen = null
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
+function toggleMenu(menuQuery) {
+    if (currentlyOpen != null) {
+        if (currentlyOpen == menuQuery) {
+            $("#" + currentlyOpen).slideUp("slow");
+            currentlyOpen = null
+        }
+        else if (menuQuery == "home") {
+            $("#" + currentlyOpen).slideUp("slow");
+            currentlyOpen = null
+        }
+        else {
+            $("#" + currentlyOpen).slideUp("slow", function() {
+                $("#" + menuQuery).slideDown("slow");
+            });
+            currentlyOpen = menuQuery
+        }
+    }
+    else if (menuQuery == "home") {
+        null
+    }
+    else {
+        $("#" + menuQuery).slideDown("slow");
+        currentlyOpen = menuQuery
+    }
+}
+
+$("#home-menu").click(function () {
+    toggleMenu("home")
+})
+
+$("#portfolio-menu").click(function () {
+    toggleMenu("portfolio")
+});
+
+$("#contact-menu").click(function () {
+    toggleMenu("contact")
+});
+
+$("#about-menu").click(function () {
+    toggleMenu("about")
+});
+
+$(".menu-close").click(function (){
+    toggleMenu("home")
+})
